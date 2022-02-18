4ff2a27f56bf708f411c97bfda0699fed68980b7
WP-Stars <55444591+wp-stars-vienna@users.noreply.github.com>
2022-02-18 08:40:26 +0100

    Update wps-post-migrator.php
    Fixed issue with categories.

--- wps-post-migrator/wps-post-migrator.php
+++ wps-post-migrator/wps-post-migrator.php
@@ -54,7 +54,7 @@ function wps_post_migrator_install() {
 	
 	$sql = "CREATE TABLE $table_name (
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
-        original_id int NOT NULL,
+        	original_id int NOT NULL,
 		new_id int NOT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
